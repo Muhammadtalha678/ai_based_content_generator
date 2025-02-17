47f180e4c38e064a6127af9eb6cdf2feff9977a8
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-18 00:29:55 +0500

    allow cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,16 @@
 //this file is create to divide the code into modules like this file connect api controllers etc
 // Express app setup karega
 import express from 'express'
-
+import cors from 'cors'
+import { frontend_url } from './configs/config.js'
 const app = express()
+app.use(cors(
+    {
+        origin: [frontend_url,"http://localhost:3000/"],
+        methods: ["GET","POST","PUT","DELETE"],
+        credentials:true
+    }
+))
 app.use(express.json())
 
 app.get('/', (req,res) => {
--- backend/src/configs/config.js
+++ backend/src/configs/config.js
@@ -2,5 +2,6 @@ import 'dotenv/config'
 
 const port = process.env.PORT
 const monogo_uri = process.env.MONOGO_URI
+const frontend_url = process.env.Frontend_Url
 
-export {port,monogo_uri}
+export {port,monogo_uri,frontend_url}
